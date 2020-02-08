d3ceb7c3db6434904ad50caef8aad5e7b96d9db8
ryu <ryu@ryunoMacBook-Pro.local>
2020-02-08 17:26:47 +0900

    HeunMethod complete

--- Runge–Kutta.py
+++ Runge–Kutta.py
@@ -1,33 +1,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
-"""
-4次のルンゲ-クッタ法による1階常微分方程式
-The fourth-order Runge-Kutta method for the 1st order ordinary differencial equation
 
-dx/dt = -x^3+sin(t)を解く
-"""
 
-def f(x,t):
-    return -x**3 +np.sin(t)
+def Heunmethod(a: float, b: float, n: int,y0:float, f) -> list:
+    """
+    オイラー法　y'=f(x,y) のyを逐次計算し、listで返す
+    :param a: xの下限
+    :param b: xの上限
+    :param n: 分割数
+    :param y0: yの初期値
+    :param f: 微分方程式の関数
+    :return: yの計算結果list
+    """
+    h = (b - a) / n
+    x = np.linspace(a, b, n)
+    y = np.zeros(n)
+    tmp_y = y0
+    k1=0
+    k2=0
+    for i in range(n):
+        k1=f(x[i],tmp_y)
+        k2=f(x[i]+h,tmp_y+h*k1)
+        tmp_y = tmp_y + h*(k1+k2)/2
+        y[i] = tmp_y
+    return x,y
 
-a = 0.0
-b = 10.0
-N = 100
-h = (b-a)/N
 
-tpoints = np.arange(a,b,h)
-xpoints = []
-x = 0.0
 
-for t in tpoints:
-    xpoints.append(x)
-    k1 = h*f(x,t)
-    k2 = h*f(x+0.5*k1, t+0.5*h)
-    k3 = h*f (x+0.5*k2, t+0.5*h)
-    k4 = h*f(x+k3, t+h)
-    x += (k1+2*k2+2*k3+k4)/6
+def func(x:float,y:float)->float:
+    """
+    常微分方程式の関数　y'=f(x,y)
+    :param x:
+    :param y:
+    :return: f(x,y)
+    """
+    return x+y
 
-plt.plot (tpoints, xpoints)
-plt.xlabel("t")
-plt.ylabel("x(t)")
-plt.show()
+if __name__=='__main__':
+    x,y=Heunmethod(0,1,10,1,func)
+    print(y)
+    plt.plot(x,y)
+    plt.show()
